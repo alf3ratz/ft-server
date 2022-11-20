e6c7554e7e99075bd2b4d0b045be24eaebe7ac53
alf3ratz <apetropavlovskij@yandex.ru>
2022-11-21 02:21:57 +0300

    [ft-0004] Fix some problems and remove dependency

--- src/main/java/ru/alferatz/ftserver/repository/TravelRepository.java
+++ src/main/java/ru/alferatz/ftserver/repository/TravelRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.alferatz.ftserver.repository.entity.TravelEntity;
 
 public interface TravelRepository extends JpaRepository<TravelEntity, Long> {
+
 }
