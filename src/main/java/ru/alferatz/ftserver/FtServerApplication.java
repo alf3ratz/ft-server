42e48a5b99a211a594a1dca088cf5613323a7210
alf3ratz <apetropavlovskij@yandex.ru>
2022-11-20 04:17:30 +0300

    [ft-0001] Create application

---
+++ src/main/java/ru/alferatz/ftserver/FtServerApplication.java
@@ -0,0 +1,13 @@
+package ru.alferatz.ftserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FtServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ru/alferatz/ftserver/FtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alferatz.ftserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
