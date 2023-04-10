ccaef212805bf1934d314598a630367a0d211742
alf3ratz <apetropavlovskij@yandex.ru>
2023-04-11 02:03:45 +0300

    [master-branch] Рефакторинг пакета для деплоя приложения

--- deployment/src/main/java/ru/alferatz/deployment/FtServerApplication.java
+++ deployment/src/main/java/ru/alferatz/deployment/FtServerApplication.java
@@ -1,13 +1,13 @@
-package ru.alferatz.travels;
+package ru.alferatz.deployment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TravelsApplication {
+public class FtServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(TravelsApplication.class, args);
+    SpringApplication.run(FtServerApplication.class, args);
   }
 
 }
