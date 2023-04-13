e80b8acf506f5ee7a1f71789976d95ae225d5a9f
alf3ratz <apetropavlovskij@yandex.ru>
2023-04-13 05:45:54 +0300

    [master-chat] Configure

--- src/main/java/ru/alferatz/ftserver/config/WebSocketConfig.java
+++ src/main/java/ru/alferatz/ftserver/config/WebSocketConfig.java
@@ -1,45 +1,45 @@
-package ru.alferatz.ftserver.config;
-
-import lombok.*;
-//import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-//@Log4j2
-@RequiredArgsConstructor
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-  @Value("${spring.rabbitmq.host}")
-  private final String host;
-
-  @Value("${spring.rabbitmq.username}")
-  private final String username;
-
-  @Value("${spring.rabbitmq.password}")
-  private final String password;
-
-  public static final String TOPIC_DESTINATION_PREFIX = "/topic/";
-  public static final String REGISTRY = "/ws";
-
-  @Override
-  public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint(REGISTRY)
-        .withSockJS();
-  }
-
-  @Override
-  public void configureMessageBroker(MessageBrokerRegistry config) {
-    config.enableStompBrokerRelay(TOPIC_DESTINATION_PREFIX)
-        .setRelayHost(host)
-        .setClientLogin(username)
-        .setClientPasscode(password)
-        .setSystemLogin(username)
-        .setSystemPasscode(password);
-  }
-}
+//package ru.alferatz.ftserver.config;
+//
+//import lombok.*;
+////import lombok.extern.log4j.Log4j2;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+////@Log4j2
+//@RequiredArgsConstructor
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+//
+//  @Value("${spring.rabbitmq.host}")
+//  private final String host;
+//
+//  @Value("${spring.rabbitmq.username}")
+//  private final String username;
+//
+//  @Value("${spring.rabbitmq.password}")
+//  private final String password;
+//
+//  public static final String TOPIC_DESTINATION_PREFIX = "/topic/";
+//  public static final String REGISTRY = "/ws";
+//
+//  @Override
+//  public void registerStompEndpoints(StompEndpointRegistry registry) {
+//    registry.addEndpoint(REGISTRY)
+//        .withSockJS();
+//  }
+//
+//  @Override
+//  public void configureMessageBroker(MessageBrokerRegistry config) {
+//    config.enableStompBrokerRelay(TOPIC_DESTINATION_PREFIX)
+//        .setRelayHost(host)
+//        .setClientLogin(username)
+//        .setClientPasscode(password)
+//        .setSystemLogin(username)
+//        .setSystemPasscode(password);
+//  }
+//}
