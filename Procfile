b5b16516033866935330d1dc220fec92eaf492b3
alf3ratz <apetropavlovskij@yandex.ru>
2023-04-16 21:30:11 +0300

    [master-chat] Add deployment with properties

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/*.jar -Dspring.config.location=file:${home}/src/main/resources/application-stage.yml
--- src/main/java/ru/alferatz/ftserver/FtServerApplication.java
+++ src/main/java/ru/alferatz/ftserver/FtServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
-@PropertySources({@PropertySource(value = {"file:${application.properties}"})})
 public class FtServerApplication {
 
   public static void main(String[] args) {
