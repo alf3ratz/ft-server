f55da632d221235be440ffe5bdd1f92ff3f98d0a
alf3ratz <apetropavlovskij@yandex.ru>
2023-04-16 21:18:15 +0300

    [master-chat] Add deployment with properties

--- src/main/java/ru/alferatz/ftserver/FtServerApplication.java
+++ src/main/java/ru/alferatz/ftserver/FtServerApplication.java
@@ -2,14 +2,17 @@ package ru.alferatz.ftserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
+@PropertySources({@PropertySource(value = {"file:${application.properties}"})})
 public class FtServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FtServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(FtServerApplication.class, args);
+  }
 
 }
