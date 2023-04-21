96aabec0f77f421739fce1c93e024179444b109f
alf3ratz <apetropavlovskij@yandex.ru>
2023-04-21 05:04:52 +0300

    [auth] auth

--- src/main/java/ru/alferatz/ftserver/auth/AuthController.java
+++ src/main/java/ru/alferatz/ftserver/auth/AuthController.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin
 public class AuthController {
 
-  @GetMapping("/login")
+  @GetMapping("/hse_redirect")
   public String login(Principal principal) {
       return "";
   }
