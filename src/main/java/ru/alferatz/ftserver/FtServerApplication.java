7a26fa07adc169ab66f342787026d9481f9ed7b0
alf3ratz <apetropavlovskij@yandex.ru>
2023-05-27 23:49:11 +0300

    [master] fix bug

--- src/main/java/ru/alferatz/ftserver/FtServerApplication.java
+++ src/main/java/ru/alferatz/ftserver/FtServerApplication.java
@@ -1,38 +1,38 @@
-package ru.alferatz.ftserver;
-
-import java.util.Arrays;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-@EnableWebMvc
-@SpringBootApplication
-public class FtServerApplication {
-
-  public static void main(String[] args) {
-    SpringApplication.run(FtServerApplication.class, args);
-  }
-
-  @Bean
-  CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-        "https://6466e6d30012ed2bf9aa3748--serene-semolina-0dd3f7.netlify.app"));
-    configuration.setAllowedMethods(
-        Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-    configuration.setAllowCredentials(true);
-    configuration.setAllowedHeaders(Arrays
-        .asList("Authorization", "Requestor-Type", "Access-Control-Allow-Origin", "Content-Type"));
-    configuration.setExposedHeaders(Arrays
-        .asList("X-Get-Header", "Access-Control-Allow-Origin", "X-Content-Type-Options",
-            "X-Frame-Options"));
-    configuration.setMaxAge(3600L);
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
-}
+//package ru.alferatz.ftserver;
+//
+//import java.util.Arrays;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//@EnableWebMvc
+//@SpringBootApplication
+//public class FtServerApplication {
+//
+//  public static void main(String[] args) {
+//    SpringApplication.run(FtServerApplication.class, args);
+//  }
+//
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//    CorsConfiguration configuration = new CorsConfiguration();
+//    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000",
+//        "https://6466e6d30012ed2bf9aa3748--serene-semolina-0dd3f7.netlify.app"));
+//    configuration.setAllowedMethods(
+//        Arrays.asList("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
+//    configuration.setAllowCredentials(true);
+//    configuration.setAllowedHeaders(Arrays
+//        .asList("Authorization", "Requestor-Type", "Access-Control-Allow-Origin", "Content-Type"));
+//    configuration.setExposedHeaders(Arrays
+//        .asList("X-Get-Header", "Access-Control-Allow-Origin", "X-Content-Type-Options",
+//            "X-Frame-Options"));
+//    configuration.setMaxAge(3600L);
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", configuration);
+//    return source;
+//  }
+//}
