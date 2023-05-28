570d913e7dfdf52f5df840143fc055bc370c9d28
alf3ratz <apetropavlovskij@yandex.ru>
2023-05-28 03:33:18 +0300

    [master] add coords

--- src/main/java/ru/alferatz/ftserver/auth/AuthController.java
+++ src/main/java/ru/alferatz/ftserver/auth/AuthController.java
@@ -1,6 +1,8 @@
 package ru.alferatz.ftserver.auth;
 
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +10,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.alferatz.ftserver.model.request.AuthResponse;
 
 @RequestMapping("")
 @RestController
@@ -22,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
   private final AuthService authService;
+  private final Map<String, String> emailToSession = new HashMap<>();
 
   @GetMapping("/auth/hse_redirect")
   public void redirect(@RequestParam("code") String token) {
@@ -42,41 +47,26 @@ public class AuthController {
     System.out.println("был тут");
   }
 
-  @RequestMapping("/welcome")
-  public String simpleRequest(HttpServletResponse response) {
-    // Get the username of the currently logged in user
-    // this method is defined below
-    Optional<String> username = getUsernameFromSecurityContext();
-    if (username.isEmpty()) {
-      // if user information cannot be obtained, return
-      // a 403 status
-      response.setStatus(HttpStatus.FORBIDDEN.value());
-      return "error";
-    }
-
-    // show the welcome messages with the users Github username
-    return "welcome " + username.get();
-  }
-
-  private Optional<String> getUsernameFromSecurityContext() {
-    // Get the security context for this request thread
-    // and get the principal object from the context
+  @RequestMapping("/auth/success")
+  public String success() {
     SecurityContext context = SecurityContextHolder.getContext();
-    Object principal = context.getAuthentication().getPrincipal();
-
-    // If the user is authenticated, the principal should be an
-    // instance of DefaultOAuth2User
-    if (!(principal instanceof DefaultOAuth2User)) {
-      // if not, return an empty value
-      return Optional.empty();
-    }
-
-    // Get the username from the DefaultOAuth2User and return
-    // the welcome message
-    String username =
-        ((DefaultOAuth2User) principal).getAttributes().get("login").toString();
-    return Optional.of(username);
+    var authDetails = (WebAuthenticationDetails) context.getAuthentication().getDetails();
+    var credentials = (MyOAuth2User) context.getAuthentication().getPrincipal();
+    var credMap = credentials.getAttributes();
+    var userName = ((String) credMap.get("username"));
+    var sessionId = authDetails.getSessionId();
+    //var res = new ObjectMapper().readValue(username, HashMap.class);
+    emailToSession.put(sessionId, (String) credMap.get("email"));
+    return String
+        .format("%s successfully authorized!\nSave value and close the window\nValue: %s",
+            userName, sessionId);
   }
 
+  @GetMapping("/auth/isLogged")
+  public AuthResponse isSuccessfullyLogged(@RequestParam String sessionId) {
+    return AuthResponse.builder()
+        .isLogged(emailToSession.containsKey(sessionId))
+        .build();
+  }
 
 }
---
+++ src/main/java/ru/alferatz/ftserver/model/request/AuthResponse.java
@@ -0,0 +1,16 @@
+package ru.alferatz.ftserver.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+  private boolean isLogged;
+}
